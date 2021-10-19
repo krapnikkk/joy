dd44b91b5d611c06ea2b873482c4a6023600e5e7
krapnik <krapnik@qq.com>
2021-10-20 01:06:27 +0800

    活动可切换账号

--- src/index/Config.ts
+++ src/index/Config.ts
@@ -1,14 +1,14 @@
 import { IActivity } from "@src/@types";
 import Goose from "./components/Activity/btgoose";
-import PetPig from "./components/Activity/PetPig";
+// import PetPig from "./components/Activity/PetPig";
 
 export const activityInfo:IActivity[] = [
     {
         "name":"天天提鹅",
         "component":Goose
     },
-    {
-        "name":"养猪猪",
-        "component":PetPig
-    }
+    // {
+    //     "name":"养猪猪",
+    //     "component":PetPig
+    // }
 ]
--- src/index/components/Activity/btgoose/index.tsx
+++ src/index/components/Activity/btgoose/index.tsx
@@ -1,13 +1,12 @@
-import { Button, Card, message, PageHeader, Radio, Switch, Tooltip } from 'antd';
-import { QuestionCircleOutlined } from "@ant-design/icons";
 import * as React from 'react';
-import { AUTO_GET_COOKIES, GET_COOKIES_SUCCESS, LOGIN } from '@src/Events';
-import { copyText, localStoragePromise, openWindow } from '@src/utils';
+import { Button, Card, Dropdown, Menu, message, PageHeader, Radio, Switch, Tooltip } from 'antd';
+import { QuestionCircleOutlined, UserOutlined, DownOutlined } from "@ant-design/icons";
+import { localStoragePromise, openWindow } from '@src/utils';
 import { IAccount, IActivityResData } from '@src/@types';
 import { Content } from 'antd/lib/layout/layout';
 import './index.css';
 import TextArea from 'antd/lib/input/TextArea';
-import { autoToWithdraw, toDailyHome, toGoldExchange } from '@src/Activity';
+import { autoToWithdraw, toDailyHome, toDailySignIn, toGoldExchange } from '@src/Activity';
 import { DateTime } from 'luxon';
 
 interface IState {
@@ -25,27 +24,12 @@ export default class Goose extends React.Component<IProps, IState, {}> {
             log: ""
         };
         // this.addEvent();
+        this.getAccountInfo();
     }
 
     public componentDidMount() {
         // console.log("componentDidMount");
     }
-
-    addEvent() {
-        chrome.runtime.onMessage.addListener((request, _sender: chrome.runtime.MessageSender, sendResponse) => {
-            console.log(request);
-            switch (request.type) {
-                case GET_COOKIES_SUCCESS:
-                    this.getAccountInfoAsync(false);
-                    message.success('获取cookies成功！');
-                    break;
-                default:
-                    console.log(request)
-                    break;
-            }
-        });
-    }
-
 
     render() {
         return (
@@ -62,7 +46,7 @@ export default class Goose extends React.Component<IProps, IState, {}> {
                 <Content>
                     <Card>
                         <section className="setting-item">
-                            <p>
+                            <section className="operation">
                                 <Button type="primary" onClick={() => {
                                     this.toDailyHome();
                                 }}>
@@ -73,23 +57,40 @@ export default class Goose extends React.Component<IProps, IState, {}> {
                                 }}>
                                     兑换积分
                                 </Button>
-                                <Button type="primary">
+                                {/* <Button type="primary"onClick={() => {
+                                    this.toGoldExchange();
+                                }}>
                                     每日签到
-                                </Button>
+                                </Button> */}
                                 <Button type="primary" onClick={() => {
                                     this.toWithdraw();
                                 }}>
                                     提鹅收蛋
                                 </Button>
-                                <Button type="primary">
+                                {/* <Button type="primary">
                                     领取任务
-                                </Button>
-                            </p>
+                                </Button> */}
+                                当前账号：<Dropdown overlay={() => {
+                                    return (<Menu onClick={this.handleMenuClick.bind(this)}>
+                                        {
+                                            this.state.accountInfo.map((account,idx) => (
+                                                <Menu.Item key={idx} icon={<UserOutlined />}>
+                                                    {account.nickname}
+                                                </Menu.Item>
+                                            ))
+                                        }
+                                    </Menu>)
+                                }} trigger={['click']}>
+                                    <Button>
+                                        全部账号 <DownOutlined />
+                                    </Button>
+                                </Dropdown>
+                            </section>
                             <TextArea rows={10} value={this.state.log} />
                         </section>
                         <section className="setting-item">
                             <p>
-                                调度开关：
+                                开启后台任务：
                                 <Tooltip
                                     placement="top"
                                     title="控制是否使用当前账号接受服务器指令调度浏览指定帖子的开关"
@@ -104,7 +105,7 @@ export default class Goose extends React.Component<IProps, IState, {}> {
                                 />
                             </p>
                             <p>
-                                调度间隔(分)：
+                                定时间隔(分)：
                                 <Tooltip
                                     placement="top"
                                     title="每次接受服务器指令调度浏览帖子的时间间隔"
@@ -118,19 +119,10 @@ export default class Goose extends React.Component<IProps, IState, {}> {
                                 value="{this.state.scheduleSpan}"
                                 disabled={false}
                             >
-                                <Radio value={1}>1</Radio>
-                                <Radio value={2}>2</Radio>
+                                <Radio value={1}>5</Radio>
+                                <Radio value={2}>10</Radio>
                             </Radio.Group>
                         </section>
-                        <p>
-                            调度时间：
-                            <Tooltip
-                                placement="top"
-                                title="指定的时间范围内接受服务器调度浏览帖子的指令(开始时间和结束时间相同即不限制时间段)"
-                            >
-                                <QuestionCircleOutlined />
-                            </Tooltip>
-                        </p>
                     </Card>
                 </Content>
             </section>
@@ -148,24 +140,46 @@ export default class Goose extends React.Component<IProps, IState, {}> {
 
     async toWithdraw() {
         let res = await autoToWithdraw() as IActivityResData;
-        let resultData = res.resultData.data;
-        let { userLevelDto, eggTotal } = resultData;
-        let { level, levelName, levelEggNum, userHaveEggNum } = userLevelDto;
-        let data = `当前等级:${level} 等级称号:${levelName} 升级进度:${userHaveEggNum}/${levelEggNum}  我的鹅蛋数量:${eggTotal}`
-        this.logOutput(data);
+        let { resultData } = res;
+        let { code, msg } = resultData;
+        let log = "";
+        if (code == "0000") {
+            let data = resultData.data;
+            let { userLevelDto, eggTotal } = data;
+            let { level, levelName, levelEggNum, userHaveEggNum } = userLevelDto;
+            log = `${msg} 当前等级:${level} 等级称号:${levelName} 升级进度:${userHaveEggNum}/${levelEggNum}  我的鹅蛋数量:${eggTotal}`
+        } else {
+            log = msg;
+        }
+        this.logOutput(log);
     }
 
-    async toGoldExchange(){
+    async toGoldExchange() {
         let res = await toGoldExchange() as IActivityResData;
         let resultData = res.resultData.data;
         let data = "";
-        if(resultData){
-            let { goldTotal, cnumber,rate,availableTotal } = resultData;
+        if (resultData) {
+            let { goldTotal, cnumber, rate, availableTotal } = resultData;
             data = `兑换成功！兑换比例:${rate} 已兑金币:${cnumber} 当前金币:${goldTotal} 剩余鹅蛋数量:${availableTotal}`
-        }else{
+        } else {
             data = res.resultData.msg;
         }
         this.logOutput(data);
+    }
+
+    async toDailySignIn() {
+        let res = await toDailySignIn() as IActivityResData;
+        let { resultData } = res;
+        let { code, msg } = resultData;
+        let log = "";
+        if (code == "0000") {
+            // let data = resultData.data;
+
+        } else {
+            log = msg;
+        }
+        this.logOutput(log);
+
     }
 
     logOutput(text: string) {
@@ -178,71 +192,27 @@ export default class Goose extends React.Component<IProps, IState, {}> {
         })
     }
 
-    autoGetCK() {
-        chrome.runtime.sendMessage({
-            type: AUTO_GET_COOKIES
-        })
+    showMessage(type: string, content: string, duration: number = 1) {
+        message[type]({ content, duration });
     }
 
-    login() {
-        chrome.runtime.sendMessage({
-            type: LOGIN
-        })
-    }
-
-    copyText(index: number) {
-        let text = this.state.accountInfo[index].cookie;
-        copyText(text).then(() => {
-            this.showMessage("复制成功！");
-        })
-
-    }
-
-    showMessage(content: string, duration: number = 1) {
-        message.success({ content, duration });
-    }
-
-    getAccountInfoAsync(tips: boolean = true) {
-        localStoragePromise.get("account").then((res: { [key: string]: IAccount }) => {
+    async getAccountInfo() {
+        let accountInfo = await localStoragePromise.get("account").then((res: { [key: string]: IAccount }) => {
             let data = [];
             let { account } = res;
             for (let key in account) {
                 data.push(account[key]);
             }
-            console.log(data);
-            this.setState(
-                {
-                    accountInfo: data
-                },
-                tips ? () => {
-                    this.showMessage("获取缓存数据成功!");
-                } : null
-            );
-        })
-    }
-
-
-    deleteCookie(index: number) {
-        let curPin = this.state.accountInfo[index].curPin;
-        localStoragePromise.get("account").then((res: any) => {
-            let { account } = res;
-            delete account[curPin];
-            localStoragePromise.set({
-                account
-            })
-            this.showMessage("删除成功！");
-            this.getAccountInfoAsync(false);
-        })
-    }
-
-    clearAllCookie() {
-        localStoragePromise.set({
-            account: {}
+            return data;
         });
-        this.showMessage("已清空所有账号cookie");
-        this.setState(
-            {
-                accountInfo: []
-            });
+        if (accountInfo.length == 0) {
+            this.showMessage("warning", "请先导入账号！")
+        }
+        this.setState({
+            accountInfo
+        })
+    }
+    handleMenuClick(idx:number) {
+        console.log(idx);
     }
 }
