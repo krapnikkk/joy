ee90b2e99123f740d5a7b36ac06eb3818a481fc6
krapnik <krapnik@qq.com>
2021-10-19 23:53:51 +0800

    调整路由为HashRouter模式

--- src/index/index.tsx
+++ src/index/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 if (process.env.NODE_ENV !== 'development') {
     Sentry.init({
         dsn: "https://b24c68a8384646f0b2b8bc4237e7f125@o643154.ingest.sentry.io/5996737",
@@ -16,4 +16,4 @@ if (process.env.NODE_ENV !== 'development') {
     });
 }
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("index"));
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById("index"));
